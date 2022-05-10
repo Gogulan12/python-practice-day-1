eef3981b59d2496fd66025a6d1b68c033bfcca66
Gogulan12 <69683898+Gogulan12@users.noreply.github.com>
2022-05-10 13:00:43 -0400

    Add files via upload

---
+++ Python-Day1-Variables-in-Python/main.py
@@ -0,0 +1,65 @@
+
+
+
+print("Hello world")
+print("Hello world\nHello world\nHello world")
+print("Hello" + " " + "Angela")
+
+
+########### Exercise 1 Printing ###################
+
+print ("Day 1 - Python Print Function")
+print ("The function is declared like this:")
+print ("print('what to print')")
+
+
+########### Exercise 2 Debugging Practice ###################
+
+# #Fix the code below 👇
+#
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the " "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+########### Input Function ###################
+# input("what is your name?")
+
+# print ("Hello " + input("what is your name?") + "!")
+
+
+########### Exercise 3 Input Function ###################
+# name = input("What is your name?")
+# print (len(name))
+
+
+print(len(input("What is your name?")))
+
+
+########### Exercise 4 Variables ###################
+
+# # 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# # 🚨 Don't change the code above 👆
+#
+# ####################################
+# #Write your code below this line 👇
+#
+# z=a
+# a=b
+# b=z
+#
+#
+# #Write your code above this line 👆
+# ####################################
+#
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
